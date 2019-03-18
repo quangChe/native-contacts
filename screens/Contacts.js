46bd456eba668ec5f8d3aba3ecef1a2f27acd653
quangChe <quang.n.che@gmail.com>
2019-03-18 14:39:49 -0700

    Contacts Screen
    Added a Contacts screen to the app. This container component is
    responsible for fetching contact mockdata and rendering a flat list of
    presentation ContactListItem components for each contact. Two
    improvements need to be made until the Contacts screen is finalized:
    
    1. Set upper and lower boundaries for the screen.
    2. Provide required props for ContactListItem (onPress).

---
+++ screens/Contacts.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  StyleSheet, 
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+
+import ContactListItem from '../components/ContactListItem';
+import {fetchContacts} from '../utils/api';
+
+const keyExtractor = ({phone}) => phone;
+
+export default class Contacts extends React.Component {
+  state = {
+    contacts: [],
+    loading: true,
+    error: false
+  };
+
+  async componentDidMount() {
+    try {
+      const contacts = await fetchContacts();
+      this.setState({contacts, loading: false, error: false});
+    } catch (err) {
+      this.setState({loading: false, error: true});
+    }
+  }
+
+  renderContact = ({item}) => {
+    const {name, avatar, phone} = item;
+
+    return(
+      <ContactListItem name={name} avatar={avatar} phone={phone}/>
+    );
+  }
+
+  render() {
+    const {loading, contacts, error} = this.state;
+    const sortedContacts = contacts.sort((a, b) => a.name.localeCompare(b.name));
+
+    return( 
+      <View style={styles.container}>
+        {loading && <ActivityIndicator size="large"/>}
+
+        {error && <Text>Unexpected error...</Text>}
+
+        {!loading && 
+          !error && (
+            <FlatList
+              data={sortedContacts}
+              keyExtractor={keyExtractor}
+              renderItem={this.renderContact}>
+            </FlatList>
+          )
+        }
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    flex: 1,
+  },
+});

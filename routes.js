01c8b01dfbf24352d5363380604e3142c8ab1e84
quangChe <quang.n.che@gmail.com>
2019-03-18 17:23:38 -0700

    Favorites Screen
    Completed a Favorites screen to grab and filter mock data to render
    a flat list of contact thumbnails. Also added this screen to the
    navigation routes of the app.

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ import {createStackNavigator, createAppContainer} from 'react-navigation';
 
 import Contacts from './screens/Contacts';
 import Profile from './screens/Profile';
+import Favorites from './screens/Favorites';
 
 const AppNavigator =  createStackNavigator(
   {
@@ -11,9 +12,12 @@ const AppNavigator =  createStackNavigator(
     Profile: {
       screen: Profile,
     },
+    Favorites: {
+      screen: Favorites,
+    },
   },
   {
-    initialRouteName: 'Contacts'
+    initialRouteName: 'Favorites'
   }
 );
 
---
+++ screens/Favorites.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  StyleSheet, 
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+
+import {fetchContacts} from '../utils/api';
+
+import ContactThumbnail from '../components/ContactThumbnail';
+
+const keyExtractor = ({phone}) => phone;
+
+export default class Favorites extends React.Component {
+  static navigationOptions = {
+    title: 'Favorites',
+  };
+
+  state = {
+    contacts: [],
+    loading: true,
+    error: false,
+  };
+
+  async componentDidMount() {
+    try {
+      const contacts = await fetchContacts();
+      this.setState({contacts, loading: false, error: false});
+    } catch (err) {
+      this.setState({loading: false, error: true});
+    }
+  }
+
+  renderFavoriteThumbnail = ({item}) => {
+    const {navigation: {navigate}} = this.props;
+    const {avatar} = item;
+
+    return (
+      <ContactThumbnail
+        avatar={avatar}
+        onPress={() => navigate('Profile', {contact: item})}/>
+    );
+  }
+
+  render() {
+    const {loading, contacts, error} = this.state;
+    const favorites = contacts.filter(contact => contact.favorite);
+
+    return (
+      <View style={styles.container}>
+        {loading && <ActivityIndicator size="large"/>}
+
+        {error && <Text>Unexpected error...</Text>}
+
+        {!loading && !error && (
+          <FlatList
+            data={favorites}
+            keyExtractor={keyExtractor}
+            numColumns={3}
+            contentContainerStyle={styles.list}
+            renderItem={this.renderFavoriteThumbnail}/>
+        )}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  list: {
+    alignItems: 'center',
+  },
+});
